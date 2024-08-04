532fd302b9212ad6b75a79de3a37e438f287a2b7
brunocesarr <brunoc.15@hotmail.com>
2024-08-03 23:25:05 -0300

    #fix: Change root files.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,8 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
+      <Head />
       <body>
-        <Head />
         <Main />
         <NextScript />
       </body>
